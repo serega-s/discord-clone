52e12745f7249e4f29da6c930c7c26201091e31a
serega-s <comp.it.05@inbox.ru>
2021-11-03 14:54:45 +0300

    added notification views

---
+++ notification/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Notification
+from authy.serializers import UserSerializer
+from server.serializers import ServerSerializer
+
+class NotificationSerializer(serializers.ModelSerializer):
+    to_user = UserSerializer(read_only=True)
+    from_user = UserSerializer(read_only=True)
+    to_server = ServerSerializer(read_only=True)
+
+    class Meta:
+        model = Notification
+        fields = '__all__'
